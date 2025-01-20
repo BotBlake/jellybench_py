340703470e9e242ecaaac6441cd521591982d34f
sevenrats <sevenrats@tutanota.com>
2025-01-20 05:15:24 -0500

    remove click from api

--- jellybench_py/api.py
+++ jellybench_py/api.py
@@ -19,26 +19,25 @@
 ##########################################################################################
 from json import JSONDecodeError, load, dumps
 
-import click
 import requests
 
 
 def getPlatform(server_url: str) -> list:
-    click.echo("| Fetch Supported Platforms...", nl=False)
+    print("| Fetch Supported Platforms...", nl=False)
     platforms = None
     try:
         response = requests.get(f"{server_url}/api/v1/TestDataApi/Platforms")
         if response.status_code == 200:
-            click.echo(" success!")
+            print(" success!")
             platforms = response.json()
         else:
-            click.echo(" Error")
-            click.echo(f"ERROR: Server replied with {response.status_code}")
-            click.pause("Press any key to exit")
+            print(" Error")
+            print(f"ERROR: Server replied with {response.status_code}")
+            input("Press any key to exit")
             exit()
     except Exception:
-        click.echo(" Error")
-        click.echo("ERROR: No connection to Server possible")
+        print(" Error")
+        print("ERROR: No connection to Server possible")
         exit()
     platforms = platforms["platforms"]
     return platforms
@@ -46,27 +45,27 @@ def getPlatform(server_url: str) -> list:
 
 def getTestData(platformID: str, platforms_data: list, server_url: str) -> tuple:
     valid = True
-    click.echo("| Loading tests... ", nl=False)
+    print("| Loading tests... ", nl=False)
 
     # DevMode File Loading
     if platformID == "local" and platforms_data == "local":
         try:
             with open(server_url, "r") as file:
                 data = load(file)
-                click.echo(" success!")
+                print(" success!")
                 return valid, data
         except JSONDecodeError:
-            click.echo(" Error")
-            click.echo()
-            click.echo(
+            print(" Error")
+            print()
+            print(
                 "ERROR: Failed to decode JSON. Please check the file format.", err=True
             )
-            click.pause("Press any key to exit")
+            input("Press any key to exit")
             exit()
         except Exception as e:
-            click.echo(" Error")
-            click.echo()
-            click.echo(f"ERROR: {e}", err=True)
+            print(" Error")
+            print()
+            print(f"ERROR: {e}", err=True)
             exit()
         return False, None
 
@@ -75,9 +74,9 @@ def getTestData(platformID: str, platforms_data: list, server_url: str) -> tuple
         if platform["id"] == platformID and platform["supported"]:
             current_platform = platform["id"]
     if not current_platform:
-        click.echo(" Error")
-        click.echo("ERROR: Your Platform isnt Supported.")
-        click.pause("Press any key to exit")
+        print(" Error")
+        print("ERROR: Your Platform isnt Supported.")
+        input("Press any key to exit")
         exit()
 
     try:
@@ -85,27 +84,27 @@ def getTestData(platformID: str, platforms_data: list, server_url: str) -> tuple
             f"{server_url}/api/v1/TestDataApi?platformId={current_platform}"
         )
         if response.status_code == 200:
-            click.echo(" success!")
+            print(" success!")
             test_data = response.json()
         elif response.status_code == 429:
-            click.echo(" Error")
-            click.echo(f"ERROR: Server replied with {response.status_code}")
+            print(" Error")
+            print(f"ERROR: Server replied with {response.status_code}")
             ratelimit_time = response.headers["retry-after"]
-            click.echo(f"Ratelimited: Retry in {ratelimit_time}s")
+            print(f"Ratelimited: Retry in {ratelimit_time}s")
             exit(1)
         else:
-            click.echo(" Error")
-            click.echo(f"ERROR: Server replied with {response.status_code}")
-            click.pause("Press any key to exit")
+            print(" Error")
+            print(f"ERROR: Server replied with {response.status_code}")
+            input("Press any key to exit")
             exit()
     except Exception:
-        click.echo(" Error")
-        click.echo("ERROR: No connection to Server possible")
+        print(" Error")
+        print("ERROR: No connection to Server possible")
         exit()
     return valid, test_data
 
 def upload(server_url: str, data: dict):
-    click.echo("| Uploading to Server... ", nl=False)
+    print("| Uploading to Server... ", nl=False)
     api_url = f"{server_url}/api/v1/SubmissionApi"
     headers = {
         'Content-Type': 'application/json'
