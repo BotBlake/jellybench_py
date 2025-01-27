3fa5213c438811ad29b4a156dad582639f472cb1
BotBlake <B0TBlake@protonmail.com>
2025-01-27 16:18:43 +0100

    build test_cmd for linux

--- jellybench_py/core.py
+++ jellybench_py/core.py
@@ -260,10 +260,16 @@ def benchmark(ffmpeg_cmd: str, debug_flag: bool, prog_bar) -> tuple:
 
 def check_driver_limit(device: dict, ffmpeg_binary: str, gpu_idx: int):
     def build_test_cmd(worker_ammount: int, ffmpeg_binary: str, gpu_arg) -> str:
-        base_cmd = Constants.NVENC_TEST_WINDOWS.BASE_CMD.format(
-            ffmpeg=ffmpeg_binary, gpu=gpu_arg
-        )
-        worker_base = Constants.NVENC_TEST_WINDOWS.WORKER_CMD
+        if hwi.platform.system == "windows":
+            base_cmd = Constants.NVENC_TEST_WINDOWS.BASE_CMD.format(
+                ffmpeg=ffmpeg_binary, gpu=gpu_arg
+            )
+            worker_base = Constants.NVENC_TEST_WINDOWS.WORKER_CMD
+        else:
+            base_cmd = Constants.NVENC_TEST_LINUX.BASE_CMD.format(
+                ffmpeg=ffmpeg_binary, gpu=gpu_arg
+            )
+            worker_base = Constants.NVENC_TEST_LINUX.WORKER_CMD
         worker_commands = []
         for i in range(1, worker_ammount + 1):
             bitrate = f"{i}M"
