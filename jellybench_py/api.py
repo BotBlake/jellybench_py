ad8383cb9b2e9ce6d9f5abc8c31ef71994cee251
BotBlake <B0TBlake@protonmail.com>
2025-03-11 21:22:43 +0100

    re-add license

--- jellybench_py/api.py
+++ jellybench_py/api.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+# jellybench_py.api.py
+# A transcoding hardware benchmarking client (for Jellyfin)
+#    Copyright (C) 2024 BotBlake <B0TBlake@protonmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+##########################################################################################
 from json import JSONDecodeError
 from typing import Any, Dict, List
 
